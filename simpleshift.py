e22346930ec35e951faf73a28472589380cb27f6
Ernest Bird <159193034+ernestbird@users.noreply.github.com>
2024-02-17 20:26:52 -0500

    Add files via upload

--- simpleshift.py
+++ simpleshift.py
@@ -2,7 +2,9 @@
 #this program runs with arguments:
 #script name, old audio name, new audio name, semitones (1, +4, or -4 etc.)
 
+import sys
 from sys import argv
+import time
 from pathlib import Path
 import librosa
 import soundfile as sf
@@ -10,15 +12,13 @@ from pydub import AudioSegment
 
 script, old_audio, new_audio, semitones = argv
 
+print("Transposing. Please wait.")
+
 y, sr = librosa.load(old_audio, sr=None, mono=False)
-
 shift = librosa.effects.pitch_shift(y, sr=sr, n_steps=semitones)
-
 sf.write(new_audio, shift.T, sr, format='wav', subtype='PCM_24')
-
 new_audio_mp3 = f"{Path(new_audio).stem}.mp3"
-
 AudioSegment.from_wav(new_audio).export(new_audio_mp3, format='mp3')
 
 print(f"""Finished transposing the [{old_audio}]. 
-      The new transposed wav file has been written as [{new_audio}], as well as the mp3 file as [{new_audio_mp3}]""")
+The new transposed wav file has been written as [{new_audio}], as well as the mp3 file as [{new_audio_mp3}]""")
